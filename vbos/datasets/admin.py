e4222323c33fe2f7cda9a74828ddd82478fff6e5
Wille Marcel <wille.yyz@gmail.com>
2025-09-02 10:35:11 -0500

    Add admin view to import VectorItem from geojson files (#4)

--- vbos/datasets/admin.py
+++ vbos/datasets/admin.py
@@ -1,8 +1,10 @@
 import csv
+import json
 from io import TextIOWrapper
 
 from django.contrib.gis import admin
 from django.contrib import messages
+from django.contrib.gis.geos.geometry import GEOSGeometry
 from django.shortcuts import render, redirect, reverse
 from django.urls import path
 
@@ -13,7 +15,7 @@ from .models import (
     VectorDataset,
     VectorItem,
 )
-from .forms import CSVUploadForm
+from .forms import CSVUploadForm, GeoJSONUploadForm
 
 
 @admin.register(RasterDataset)
@@ -28,7 +30,77 @@ class VectorDatasetAdmin(admin.ModelAdmin):
 
 @admin.register(VectorItem)
 class VectorItemAdmin(admin.GISModelAdmin):
-    list_display = ["id", "metadata"]
+    list_display = ["id", "dataset", "metadata"]
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                "upload-file/",
+                self.admin_site.admin_view(self.import_file),
+                name="datasets_vectoritem_import_file",
+            ),
+        ]
+        return custom_urls + urls
+
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context["upload_file"] = reverse("admin:datasets_vectoritem_import_file")
+        return super().changelist_view(request, extra_context=extra_context)
+
+    def import_file(self, request):
+        if request.method == "POST":
+            form = GeoJSONUploadForm(request.POST, request.FILES)
+            if form.is_valid():
+                uploaded_file = request.FILES["file"]
+
+                # Check if the file is a CSV
+                if not uploaded_file.name.endswith(".geojson"):
+                    messages.error(request, "Please upload a GeoJSON file")
+                    return redirect("admin:datasets_vectoritem_import_file")
+
+                try:
+                    decoded_file = TextIOWrapper(uploaded_file.file, encoding="utf-8")
+                    geojson_content = json.loads(decoded_file.read())
+
+                    created_count = 0
+                    error_count = 0
+
+                    for item in geojson_content["features"]:
+                        try:
+                            VectorItem.objects.create(
+                                dataset=form.cleaned_data["dataset"],
+                                metadata=item["properties"],
+                                geometry=GEOSGeometry(json.dumps(item["geometry"])),
+                            )
+                            created_count += 1
+                        except Exception as e:
+                            print(e)
+                            error_count += 1
+
+                    if created_count > 0:
+                        messages.success(
+                            request, f"Successfully created {created_count} new records"
+                        )
+
+                    if error_count > 0:
+                        messages.warning(
+                            request, f"Failed to create {error_count} items."
+                        )
+
+                except Exception as e:
+                    messages.error(request, f"Error processing GeoJSON: {str(e)}")
+
+                return redirect("admin:datasets_vectoritem_import_file")
+        else:
+            form = GeoJSONUploadForm()
+
+        context = {
+            "form": form,
+            "opts": self.model._meta,
+            "title": "Import GeoJSON File",
+        }
+        return render(request, "admin/file_upload.html", context)
 
 
 @admin.register(TabularDataset)
--- vbos/datasets/forms.py
+++ vbos/datasets/forms.py
@@ -1,9 +1,16 @@
 from django import forms
-from .models import TabularDataset
+from .models import TabularDataset, VectorDataset
 
 
 class CSVUploadForm(forms.Form):
     file = forms.FileField(label="File")
     dataset = forms.ModelChoiceField(
         queryset=TabularDataset.objects.all(), empty_label="Select a dataset"
+    )
+
+
+class GeoJSONUploadForm(forms.Form):
+    file = forms.FileField(label="File")
+    dataset = forms.ModelChoiceField(
+        queryset=VectorDataset.objects.all(), empty_label="Select a dataset"
     )

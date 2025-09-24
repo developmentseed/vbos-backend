73fd1c8965a9399e6f66a8b9a9c0e9067fac1a73
Wille Marcel <wille.yyz@gmail.com>
2025-09-24 14:06:05 -0300

    Add Tabular and Vector data filters based on data/metadata JSON fields

--- vbos/datasets/test/test_tabular_views.py
+++ vbos/datasets/test/test_tabular_views.py
@@ -80,19 +80,39 @@ class TestTabularDatasetDataView(APITestCase):
         )
         TabularItem.objects.create(
             dataset=self.dataset_2,
-            data={"employed_population": 0.75, "year": 2025, "month": 1},
+            data={
+                "employed_population": 0.75,
+                "year": 2025,
+                "month": 1,
+                "region": "North",
+            },
         )
         TabularItem.objects.create(
             dataset=self.dataset_2,
-            data={"employed_population": 0.85, "year": 2024, "month": 7},
+            data={
+                "employed_population": 0.85,
+                "year": 2024,
+                "month": 7,
+                "region": "North",
+            },
         )
         TabularItem.objects.create(
             dataset=self.dataset_2,
-            data={"employed_population": 0.82, "year": 2024, "month": 1},
+            data={
+                "employed_population": 0.82,
+                "year": 2024,
+                "month": 1,
+                "region": "South",
+            },
         )
         TabularItem.objects.create(
             dataset=self.dataset_2,
-            data={"employed_population": 0.80, "year": 2023, "month": 7},
+            data={
+                "employed_population": 0.80,
+                "year": 2023,
+                "month": 7,
+                "region": "East",
+            },
         )
         self.url = reverse("datasets:tabular-data", args=[self.dataset_1.id])
 
@@ -114,3 +134,21 @@ class TestTabularDatasetDataView(APITestCase):
         assert req.data.get("results")[0]["employed_population"] == 0.75
         assert req.data.get("results")[0]["month"] == 1
         assert req.data.get("results")[0]["year"] == 2025
+
+    def test_filter_data(self):
+        url = reverse("datasets:tabular-data", args=[self.dataset_2.id])
+        req = self.client.get(url, {"filter": "year=2024"})
+        assert req.status_code == status.HTTP_200_OK
+        assert req.data.get("count") == 2
+
+        req = self.client.get(url, {"filter": "year=2024,month=1"})
+        assert req.status_code == status.HTTP_200_OK
+        assert req.data.get("count") == 1
+
+        req = self.client.get(url, {"filter": "year__gte=2024,region__icontains=south"})
+        assert req.status_code == status.HTTP_200_OK
+        assert req.data.get("count") == 1
+
+        req = self.client.get(url, {"filter": "region__icontains=north"})
+        assert req.status_code == status.HTTP_200_OK
+        assert req.data.get("count") == 2
